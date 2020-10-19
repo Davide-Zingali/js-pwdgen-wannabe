7467bd4a99a8d7f5f30e5fc5532abeb9ea6861a3
Davide <kalelc351k@gmail.com>
2020-10-19 21:29:01 +0200

    password generetor completato + bonus

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 // variabili
-
 var richiestaNome = "Come ti chiami?";
 var nomeUtente = prompt(richiestaNome);
+// console
 console.log(nomeUtente);
 
+// variabili
 var richiestaCognome = "Qual'è il tuo cognome?";
 var cognomeUtente = prompt(richiestaCognome);
+// console
 console.log(cognomeUtente);
 
+// variabili
 var richiestaColore = "Il tuo colore preferito?";
 var coloreUtente = prompt(richiestaColore);
+// console
 console.log(coloreUtente);
 
-document.getElementById('risultato').innerHTML="Ti chiami: " + nomeUtente + " " + cognomeUtente + " " + "ed il tuo colore preferito è: " + coloreUtente + "19";
+// visualizzato
+document.getElementById('risultato').innerHTML="Complimenti! La tua Password è: " + nomeUtente + cognomeUtente + coloreUtente + "19";
